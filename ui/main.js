e2dd597d849e1aaf369b36825c9c35df5dfe8ccb
sneha-NPTEL <30997610+sneha-NPTEL@users.noreply.github.com>
2017-08-19 18:24:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.InnerHTML='New value';
+
+var img=document.getElementById('madi');
+img.onclick = function () {
+   image.style.marginleft = '100px'; 
+};
